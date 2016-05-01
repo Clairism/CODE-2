b2e35fdde546cc9b170dd46b3f98524c5b92681a
Claire Hu <hum085@newschool.edu>
2016-05-01 02:37:32 -0400

    sprite & ofxSpriteRenderer add-on ready

--- Final Project/2D Game -- Code Final/src/ofApp.cpp
+++ Final Project/2D Game -- Code Final/src/ofApp.cpp
@@ -1,15 +1,17 @@
 #include "ofApp.h"
 
+bool sortVertically(basicSprite * a, basicSprite * b ) {
+    return a->pos.y > b->pos.y;
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-    
+    /*
     gui.setDefaultBackgroundColor(ofColor(255, 255, 0));
     gui.setDefaultWidth(800);
     gui.setDefaultHeight(100);
     gui.setDefaultFillColor(0);
     gui.setDefaultBorderColor(ofColor(255));
-    gui.loadFont(20);
-    gui.loadFont(<#const std::string &filename#>, <#int fontsize#>)
     //gui.setPosition(ofGetWidth()/2, ofGetHeight()/2);
     gui.setName("Dialogue");
     
@@ -17,18 +19,77 @@ void ofApp::setup(){
     
     gui.add(button.setup("Button"));
     gui.add(toggle.setup("Toggle", true));
+     */
+    
+    ofSetFrameRate(30);
+    
+    spriteRenderer = new ofxSpriteSheetRenderer(1, 10000, 0, 64); //declare a new renderer with 1 layer, 10000 tiles per layer, default layer of 0, tile size of 64
+    
+    //start with Idle anim
+    spriteRenderer->loadTexture("CharacterIdleSheet.png", 256, GL_NEAREST); // load the spriteSheetExample.png texture of size 256x256 into the sprite sheet. set it's scale mode to nearest since it's pixel art
+    
+    // turn on alpha blending.
+    ofEnableAlphaBlending();
+    ofDisableAntiAliasing();
+    
+    playerPos.x = ofGetWidth()/2;
+    playerPos.y = ofGetHeight()/2;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
+    spriteRenderer->clear(); // clear the sheet
+    spriteRenderer->update(ofGetElapsedTimeMillis()); //update the time in the renderer, this is necessary for animations to advance
+    
+    sort( sprites.begin(), sprites.end(), sortVertically ); // sorts the sprites vertically so the ones that are lower are drawn later and there for in front of the ones that are higher up
+//    spriteRenderer->addTile(0, playerPos.x, playerPos.y);
+
+    
+    if(sprites.size()>0) // if we have sprites
+    {
+        for(int i=sprites.size()-1;i>=0;i--) //go through them
+        {
+//            sprites[i]->pos.y += sprites[i]->speed; //add their speed to their y
+            //display in pos.y
+            sprites[i]->pos.y;
+            
+            /*
+            if(sprites[i]->pos.y > ofGetHeight()+16) //if they are past the bottom of the screen
+            {
+                delete sprites[i]; //delete them
+                sprites.erase(sprites.begin()+i); // remove them from the vector
+            }
+             */
+//            else
+            
+            spriteRenderer->addCenteredTile(&sprites[i]->animation, sprites[i]->pos.x, sprites[i]->pos.y); // add them to the sprite renderer (add their animation at their position, there are a lot more options for what could happen here, scale, tint, rotation, etc, but the animation, x and y are the only variables that are required)
+       }
+    }
+    
+    
+    
+    for(int i=0;i<10;i++) //lets add ten sprites every frame and fill the screen with an army
+    {
+        basicSprite * newSprite = new basicSprite(); // create a new sprite
+        
+        newSprite->pos.set(ofRandom(0,ofGetWidth())/2,ofGetHeight()/2); //set its position
+        newSprite->speed=ofRandom(10,20); //set its speed
+        newSprite->animation = suitNormalIdle; //set its animation
+        newSprite->animation.frame_duration /= newSprite->speed; //change frame duration (faster = smaller)
+        
+        newSprite->animation.index = (int)ofRandom(0,2)*8; //change the start index of our sprite. we have 4 rows of animations and our spritesheet is 8 tiles wide, so our possible start indicies are 0, 8, 16, and 24
+        sprites.push_back(newSprite); //add our sprite to the vector
+    }
+    
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
-    gui.draw();
-    
+//    gui.draw();
+    spriteRenderer->draw();
 }
 
 //--------------------------------------------------------------
--- Final Project/2D Game -- Code Final/src/ofApp.h
+++ Final Project/2D Game -- Code Final/src/ofApp.h
@@ -2,6 +2,54 @@
 
 #include "ofMain.h"
 #include "ofxGui.h"
+#include "ofxSpriteSheetRenderer.h"
+
+//create a new animation. This could be done optinally in your code andnot as a static, just by saying animation_t walkAnimation; walkAnimation.index =0, walkAnimation.frame=0 etc. I find this method the easiest though
+static animation_t walkAnimation =
+{	0,	/* .index			(int) - this is the index of the first animation frame. indicies start at 0 and go left to right, top to bottom by tileWidth on the spriteSheet		*/
+    0,	/* .frame			(int) - this is the current frame. It's an internal variable and should always be set to 0 at init													*/
+    4,	/* .totalframes		(int) - the animation length in frames																												*/
+    1,	/* .width			(int) - the width of each animation frame in tiles																									*/
+    1,	/* .height			(int) - the height of each animation frame in tiles																									*/
+    75,	/* .frameduration	(unsigned int) - how many milliseconds each frame should be on screen. Less = faster																*/
+    0,	/* .nexttick		(unsigned int) - the next time the frame should change. based on frame duration. This is an internal variable and should always be set to 0 at init	*/
+    -1,	/* .loops			(int) - the number of loops to run. -1 equals infinite. This can be adjusted at runtime to pause animations, etc.									*/
+    -1,	/* .finalindex		(int) - the index of the final tile to draw when the animation finishes. -1 is the default so total_frames-1 will be the last frame.				*/
+    1	/* .frameskip		(int) - the incrementation of each frame. 1 should be set by default. If you wanted the animation to play backwards you could set this to -1, etc.	*/
+};
+
+//a quick and dirty sprite implementation
+struct basicSprite {
+    animation_t animation;	// a variable to store the animation this sprite uses
+    ofPoint pos;			// the position on the screen this sprite will be drawn at
+    float speed;			// the speed at which this sprite moves down the screen
+};
+
+static animation_t suitNormalIdle = {
+    0,  //.index (starts drawing at index 0 on the spritesheet)
+    0,  //.frame
+    2,  //.totalframes  (2 frames in this animation)
+    1,  //.width
+    1, //.height
+    90, //.frameduration
+    0,  //.nexttick
+    -1, //.loops (infinite loops)
+    0, //.finalindex
+    1   //.frameskip
+};
+
+static animation_t casualNormalIdle = {
+    4,  //.index (changed index means pointing to another sprite on thespritesheet!)
+    0,  //.frame
+    2,  //.totalframes
+    1,  //.width
+    1,  //.height
+    90, //.frameduration
+    0,  //.nexttick
+    -1, //.loops
+    4, //.finalindex
+    1   //.frameskip
+};
 
 class ofApp : public ofBaseApp{
 
@@ -22,11 +70,19 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
-    ofxPanel gui;
+//    ofxPanel gui;
+//    
+//    ofxButton button;
+//    ofxToggle toggle;
     
-    ofxButton button;
-    ofxToggle toggle;
+    LinearTexture idleTexture;
+    LinearTexture movingTexture;
+
     
-    ofxtext
+    ofxSpriteSheetRenderer * spriteRenderer;	// our spriteRenderer
+    vector<basicSprite *> sprites;				// our vector of sprites
+    
+    ofPoint playerPos;
     
 };
+
