1fc6e221c8937c9c12e15c20b079fd82274b1207
Claire Hu <hum085@newschool.edu>
2016-05-09 08:56:03 -0400

    with footstep sound

--- Final Project/2D Game -- Code Final/src/ofApp.cpp
+++ Final Project/2D Game -- Code Final/src/ofApp.cpp
@@ -5,9 +5,9 @@ bool sortVertically(basicSprite * a, basicSprite * b ) {
 }
 
 // this method will be used to get the index of the sprite the player is standing on, or to check for possible collisions with "solid" tiles.
-int ofApp::getTileName(int x, int y) {
-    return backgrounds[y * GRIDW + x]->tileName;
-}
+//int ofApp::getTileName(int x, int y) {
+//    return backgrounds[y * GRIDW + x]->tileName;
+//}
 //--------------------------------------------------------------
 void ofApp::setup(){
     
@@ -26,26 +26,35 @@ void ofApp::setup(){
     //set position
     playerPos.x = ofGetWidth()/2;
     playerPos.y = ofGetHeight()*2/3;
+    securityX = ofGetWidth() - 50;
     
     //create "player" as an instance of our basicSprite struct, then set his position, speed, and default animation.
     player = new basicSprite();
     player->pos.set(playerPos.x, playerPos.y);
-    player->speed = 0.5;
+    player->speed = 0.03;
 
     player->animation = normalIdle;
     
+    security = new basicSprite();
+    security->animation = securityIdle;
+    
     //loop through the grid, make a new sprite background tile
-    for (int i = 0; i < GRIDH; i++) {
-        for (int j = 0; j < GRIDW; j++) {
-            basicSprite * newSprite = new basicSprite();
-            newSprite->pos.set(j*spriteRenderer->getTileSize()*BGSCALE, i*spriteRenderer->getTileSize()*BGSCALE);
-            newSprite->tileName = 24;
-            backgrounds.push_back(newSprite);
-        }
-    }
+//    for (int i = 0; i < GRIDH; i++) {
+//        for (int j = 0; j < GRIDW; j++) {
+//            basicSprite * newSprite = new basicSprite();
+//            newSprite->pos.set(j*spriteRenderer->getTileSize()*BGSCALE, i*spriteRenderer->getTileSize()*BGSCALE);
+//            newSprite->tileName = 24;
+//            backgrounds.push_back(newSprite);
+//        }
+//    }
+    
+
+    cloud.x = ofGetWidth()/2;
+    cloud.y = 50;
     
     hasSuit = false;
     isHappy = false;
+    isTalking = false;
     
     walkingLeft = false;
     walkingRight = false;
@@ -53,16 +62,29 @@ void ofApp::setup(){
     
 //text setup
     
-    //textBox.load("TextWindow.png");
+    textBox.load("TextWindow.png");
     
-    myText.init("Helvetica.dfont", 20);
-    
-    myText.setText("Welcome to my 'Egg' world. Press A, D or Left, Right Arrow to move. Space bar to change clothes. ");
-    
-    //Initially wrap the text to the screen width
-    myText.wrapTextX(2*ofGetWidth()/3);
+//    myText.init("Helvetica.dfont", 30);
+//    myText.wrapTextX(2*ofGetWidth()/3);
 
     indent = 10;
+    
+    myFont.load("SUBWT.ttf", 90);
+//sound setup
+    footstep.load("egg footstep.mp3");
+    footstep.setSpeed(1.1);
+    timer = 0;
+    
+     //create some clouds
+    for(int i = 1 ; i < 50 ; i++)
+    {
+        basicSprite * newSprite = new basicSprite();
+        newSprite->pos.set(cloud.x + i * ofRandom(500, 1000), ofRandom(cloud.y, 200));
+        newSprite->animation = bgIdle;
+        newSprite->animation.index = (int)ofRandom(26, 28);
+        clouds.push_back(newSprite);
+    }
+
 }
 
 //--------------------------------------------------------------
@@ -78,78 +100,183 @@ void ofApp::update(){
     spriteRenderer->clear();
     spriteRenderer->update(ofGetElapsedTimeMillis());
 
-    //add the Link sprite to the renderer.
-    spriteRenderer->addCenteredTile(&player->animation, player->pos.x - cameraCenter.x, player->pos.y - cameraCenter.y, 1, F_NONE, SCALE);
+    spriteRenderer->addCenteredTile(&player->animation, player->pos.x, player->pos.y, 2, F_NONE, SCALE);
+    spriteRenderer->addCenteredTile(&security->animation, securityX, playerPos.y, 1, F_NONE, SCALE);
+
+    //background (tilename, frame, x, y, layer, w, h, flipDirection, float scale, int r,g,b,alpha);
+    spriteRenderer->addCenteredTile(24, 0, ofGetWidth()/2, 150, 0, 1, 1, F_NONE, BGSCALE);
+
+    //sun
+    spriteRenderer->addCenteredTile(25, 0, 100, 100, 0, 1, 1, F_NONE, 1.2 + cos(ofGetElapsedTimef())*0.3);
+
+    //trees
+    spriteRenderer->addCenteredTile(29, 0, 60, playerPos.y - 30, 0, 1, 1, F_NONE, SCALE);
+    spriteRenderer->addCenteredTile(30, 0, 200, playerPos.y - 30, 0, 1, 1, F_NONE, SCALE);
+    spriteRenderer->addCenteredTile(31, 0, 700, playerPos.y - 30, 0, 1, 1, F_NONE, SCALE);
     
+    //clouds
+    spriteRenderer->addCenteredTile(26, 0, cloud.x, 50, 1, 1, 1, F_NONE, SCALE);
+    spriteRenderer->addCenteredTile(27, 0, cloud.x + 500, 150, 1, 1, 1, F_NONE, SCALE);
+    spriteRenderer->addCenteredTile(28, 0, cloud.x - 200, 200, 1, 1, 1, F_NONE, SCALE);
+
+    if(clouds.size()>0)
+    {
+        for(int i=clouds.size()-1;i>=0;i--)
+        {
+            clouds[i]->pos.x --; //add their speed to their y
+            
+            if(clouds[i]->pos.x <= -200){ //if they are past the left side of the screen
+                delete clouds[i];
+                clouds.erase(clouds.begin()+i);
+            }
+            else{
+                spriteRenderer->addCenteredTile(&clouds[i]->animation, clouds[i]->pos.x, clouds[i]->pos.y, 1, F_NONE, SCALE);
+            }
+        }
+    }
+
+    cloud.x --;
+    
+    //suit store
+    if(!isChanging){
+        spriteRenderer->addCenteredTile(32, 0, 420, playerPos.y - 50, 0, 1, 1, F_NONE, 3);
+    }else if(isChanging){
+        spriteRenderer->addCenteredTile(33, 0, 420, playerPos.y - 50, 0, 1, 1, F_NONE, 3);
+    }
+    
+    
+    if(playerX >= 420 - 100 && playerX <= 420 + 100){
+        canChange = true;
+    }else{
+        canChange = false;
+    }
+
     //update the player's position and animation index based on key presses.
     if (walkingLeft) {
-        player->pos.x -= player->speed * spriteRenderer->getTileSize()*SCALE;
-        player->animation.index = 8;
+        if(playerX >= 30){
+            player->pos.x -= player->speed * spriteRenderer->getTileSize()*SCALE;
+        }
+        if(!hasSuit){
+            player->animation.index = 8;
+        }else if(hasSuit){
+            player->animation.index = 20;
+        }
         player->animation.total_frames = 4;
         player->animation.frame_duration = 200;
         
 //        player->animation = walkLeft;
-
+        if(footstep.isPlaying() == false){
+            footstep.play();
+        }
     }else if (walkingRight) {
-        player->pos.x += player->speed * spriteRenderer->getTileSize()*SCALE;
-        player->animation.index = 4;
+        if(hasSuit){
+            //if(playerX <= ofGetWidth() - 30){
+                player->pos.x += player->speed * spriteRenderer->getTileSize()*SCALE;
+            //}
+        }else{
+            if(playerX <= securityX - 60){
+                player->pos.x += player->speed * spriteRenderer->getTileSize()*SCALE;
+            }
+        }
+        if(!hasSuit){
+            player->animation.index = 4;
+        }else if(hasSuit){
+            player->animation.index = 16;
+        }
         player->animation.total_frames = 4;
         player->animation.frame_duration = 200;
+        
+        if(footstep.isPlaying() == false){
+            footstep.play();
+        }
 
-    }else if(!isHappy){
-        player->animation.index = 0;
-        player->animation.total_frames = 2;
-        player->animation.frame_duration = 500;
-    }else if(isHappy){
-        player->animation.index = 2;
+    }else{
+        if(!hasSuit){
+            player->animation.index = 0;
+        }else if(hasSuit){
+            player->animation.index = 12;
+        }
         player->animation.total_frames = 2;
         player->animation.frame_duration = 500;
     }
+//    else if(isHappy){
+//        player->animation.index = 2;
+//        player->animation.total_frames = 2;
+//        player->animation.frame_duration = 500;
+//    }
     
   
     //if there are backgrounds, loop through it and add each one to the renderer.
-    if (backgrounds.size() > 0) {
-        for (int i = backgrounds.size()-1; i>=0; i--) {
-
-          spriteRenderer->addCenteredTile(backgrounds[i]->tileName, 0, backgrounds[i]->pos.x, backgrounds[i]->pos.y, 0, 1, 1, F_NONE, BGSCALE);
-
-        }
-    }
+//    if (backgrounds.size() > 0) {
+//        for (int i = backgrounds.size()-1; i>=0; i--) {
+//
+//          spriteRenderer->addCenteredTile(backgrounds[i]->tileName, 0, backgrounds[i]->pos.x, backgrounds[i]->pos.y, 0, 1, 1, F_NONE, BGSCALE);
+//
+//        }
+//    }
+    
     
     //update the background position based on the grid and the camera position.
-    for (int i = 0; i < GRIDH; i++) {
-        for (int j = 0; j < GRIDW; j++) {
-            backgrounds[i * GRIDW + j]->pos.set(j*spriteRenderer->getTileSize()*BGSCALE - cameraCenter.x, i*spriteRenderer->getTileSize()*BGSCALE - cameraCenter.y);
-        }
+//    for (int i = 0; i < GRIDH; i++) {
+//        for (int j = 0; j < GRIDW; j++) {
+//
+//             backgrounds[i * GRIDW + j]->pos.set(j*spriteRenderer->getTileSize()*BGSCALE, i*spriteRenderer->getTileSize()*BGSCALE);
+//        }
+//    }
+    
+    playerX = player -> pos.x;
+    
+    if(playerX >= securityX - 80){
+        canTalk = true;
+    }else{
+        canTalk = false;
     }
- 
-    //this is an application of how we could check the player's position against the tiles.
-    //we could use this approach to do collision detection for example.
+    
+    if(isTalking){
         
-    int tilePosX = floor((player->pos.x + (spriteRenderer->getTileSize() * SCALE)/2) / (spriteRenderer->getTileSize() * SCALE));
-    int tilePosY = floor((player->pos.y + (spriteRenderer->getTileSize() * SCALE)/2) / (spriteRenderer->getTileSize() * SCALE));
-    
-//    cout << "pos.x relative to tiles: " <<  tilePosX << ", pos.y relative to tiles: " <<  tilePosY << endl;
-//    cout << "background sprite index: " << getTileName(tilePosX, tilePosY) << endl;
-    //}
-   
-    
-    //update the camera position to focus on the player's position.
-    cameraCenter.x = player->pos.x - ofGetWindowWidth()/2;
-    cameraCenter.y = player->pos.y - ofGetWindowHeight()*2/3;
+        spriteRenderer->addCenteredTile(37, 0, ofGetWidth()/2 - myFont.stringWidth("Enjoy yourself!")/2, ofGetHeight() - 200, 3, 1, 1, F_NONE, SCALE);
 
-    
+    }
+
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    //textBox.draw(ofPoint(0, ofGetHeight()*2/3), ofGetWidth(), 120 + indent);
-
+    textBox.draw(ofPoint(ofGetWidth()/2, ofGetHeight() - 200), 200, 300);
 //    gui.draw();
     spriteRenderer->draw();
     
-    myText.setColor(0, 50, 225, 255);
-    myText.drawCenter(ofGetWidth()/2, ofGetHeight()*2/3);
+//    myText.setColor(255, 255, 255, 255);
+//    myText.drawCenter(ofGetWidth()/2, ofGetHeight() - 200);
+
+    if(isTalking){
+        //ofSetColor(255, 255, 255, 20);
+        //textBox.draw(ofPoint(ofGetWidth()/2 - myText.getWidth()/2, ofGetHeight() - 200), myText.getWidth(), myText.getHeight() * 5);
+        textBox.draw(ofPoint(ofGetWidth()/2, ofGetHeight() - 200), 200, 300);
+
+       // myText.setColor(255, 255, 0, 255);
+        myFont.load("SUBWT.ttf", 20);
+        myFont.drawString("Enjoy yourself!", ofGetWidth()/2 - myFont.stringWidth("Enjoy yourself!")/2, ofGetHeight() - 200);
+
+        if(!hasSuit){
+           // myText.setColor(0, 0, 0, 255);
+//            myText.drawCenter(ofGetWidth()/2, ofGetHeight() - 200);
+            //myFont.drawString("Security: You Can't enter without proper clothes!", ofGetWidth()/2, ofGetHeight()/2);
+
+//            myText.setText("Security: You Can't enter without proper clothes!");
+        }else if(hasSuit){
+            //myText.setText("Enjoy yourself!");
+            //myFont.s
+            //myFont.drawString("Enjoy yourself!", ofGetWidth()/2, ofGetHeight()/2);
+
+        }
+    }
+    
+    
+    if(playerX >= ofGetWidth()){
+        ofSetColor(255, 0, 0, 255);
+        myFont.drawString("You Win!", ofGetWidth()/2 - myFont.stringWidth("You Win!")/2, ofGetHeight()/3);
+    }
 
 
 }
@@ -157,7 +284,11 @@ void ofApp::draw(){
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 //space bar -- suit
-    if(key == ' '){
+    if(key == ' ' && canChange){
+        
+        isChanging = true;
+        //Changed = true;
+        
         if(!hasSuit){
             //wear suit
             hasSuit = true;
@@ -189,11 +320,22 @@ void ofApp::keyPressed(int key){
             walkingRight = true;
         }
     }
-
+    
+    //talk
+    if (key == 'e' && canTalk){
+//        cout<< canTalk << endl;
+        isTalking = true;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
+    if(key == ' '){
+        isChanging = false;
+        if(!isChanging){
+            Changed = true;
+        }
+    }
 
     if (key == 'a' || key == OF_KEY_LEFT){
         walkingLeft = false;
@@ -201,6 +343,11 @@ void ofApp::keyReleased(int key){
     
     if (key == 'd' || key == OF_KEY_RIGHT){
         walkingRight = false;
+    }
+    
+    if(key == OF_KEY_RETURN){
+
+        isTalking = false;
     }
 
 }
--- Final Project/2D Game -- Code Final/src/ofApp.h
+++ Final Project/2D Game -- Code Final/src/ofApp.h
@@ -24,7 +24,7 @@ static animation_t normalIdle = {
     0, //.finalindex
     1   //.frameskip
 };
-
+/*
 static animation_t happyIdle = {
     2,  //.index of the first animation
     0,  //.frame
@@ -63,6 +63,32 @@ static animation_t walkLeft = {
     0, //.finalindex
     1   //.frameskip
 };
+*/
+static animation_t securityIdle = {
+    35,  //.index of the first animation
+    0,  //.frame
+    2,  //.totalframes
+    1,  //.width
+    1,  //.height
+    200, //.frameduration
+    0,  //.nexttick
+    -1, //.loops
+    35, //.finalindex
+    1   //.frameskip
+};
+
+static animation_t bgIdle = {
+    0,  //.index of the first animation
+    0,  //.frame
+    1,  //.totalframes
+    1,  //.width
+    1,  //.height
+    500, //.frameduration
+    0,  //.nexttick
+    -1, //.loops
+    0, //.finalindex
+    1   //.frameskip
+};
 
 
 class ofApp : public ofBaseApp{
@@ -84,46 +110,53 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
     
-    ofPoint playerPos;
-    bool changeAnim;
+    //bool changeAnim;
 
-    //create the instance of the spriteSheetRenderer. we only need one.
     ofxSpriteSheetRenderer* spriteRenderer;
-    ofxSpriteSheetRenderer* textWindow;
 
-    //create an instance of our basicSprite struct that we'll use to store player information
-    //(this is technically a pointer to a basicSprite object but we use it essentially the same as we would if it was the object itself)
+    //create an instance of basicSprite struct (store player information)
     basicSprite * player;
-    
-    //a vector is a C++ data structure like an array, except dynamic and powerful like a Java ArrayList.
-    //we'll use this vector to store basicSprite structs for all our background tiles.
-    vector<basicSprite *> backgrounds;
-    
-    //instead of just moving the player around on a static background, lets move a "camera" position along with the player.
-    //this will move all the background tiles relative to the camera, making the world appear to move around the player's perspective.
-    ofPoint cameraCenter;
+    basicSprite * security;
+
+//    vector<basicSprite *> backgrounds;
+    vector<basicSprite *> clouds;
     
     const float SCALE = 2; //constant to store our sprite scaling value
-    const float BGSCALE = 12; //constant to store our sprite scaling value
-    const int GRIDW = 30;  //width in tiles of the background grid
-    const int GRIDH = 30;  //height in tiles of the background grid
+    const float BGSCALE = 10; //constant to store our sprite scaling value
+//    const int GRIDW = 30;  //width in tiles of the background grid
+//    const int GRIDH = 30;  //height in tiles of the background grid
     
-
+    //returns tile index
+    int getTileName(int x, int y);
+    
+    ofPoint playerPos;
+    float securityX;
+    float playerX;
+    ofPoint cloud;
+    float sunScale;
+    
+    bool canChange;
+    bool isChanging;
+    bool Changed;
     bool hasSuit;
+    
     bool isHappy;
+    
+    bool canTalk;
+    bool isTalking;
     
     bool walkingLeft;
     bool walkingRight;
     
-    //lastly, we'll make a simple function that returns which sprite a tile is at a given x and y position
-    //this is useful so we can find out which sprites the player is near so we can collide or not, for example.
-    int getTileName(int x, int y);
-    
     //dialog
+    ofTrueTypeFont myFont;
     ofxTextBlock        myText;
     TextBlockAlignment  alignment;  //constants for controlling state
     
     ofImage textBox;
     float indent;
+    
+    ofSoundPlayer footstep;
+    float timer;
 };
 
